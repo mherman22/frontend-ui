cb7db8a0064daa99d124ef56226e2992236e1bb2
mherman22 <hermanmuhereza22@gmail.com>
2024-05-18 14:55:18 +0300

    fix login component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,52 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export const content = ["./src/**/*.{html,js,jsx}"]
+export const darkMode = "class"
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      "primary1": '#2A445D',
+      "secondary1": '#415B75',
+      'orange': '#F08014',
+      'success': '#20b799',
+      'info': '#3cbade',
+      'warning': '#efb540',
+      'danger': '#fa5944',
+      'light': '#e9edf3',
+      'dark': '#222a3e',
+      'body-bg': '#F6F7FB',
+    },
+
+    transitionProperty: {
+      'height': 'height',
+      'o-m': 'opacity,margin',
+    },
+
+    minWidth: theme => ({
+      ...theme('width'),
+    }),
+
+    maxWidth: theme => ({
+      ...theme('width'),
+    }),
+
+    minHeight: theme => ({
+      ...theme('height'),
+    }),
+
+    maxHeight: theme => ({
+      ...theme('height'),
+    }),
+  },
+}
+export const plugins = [
+  require('preline/plugin'),
+  require('@tailwindcss/forms'),
+  require('@tailwindcss/typography'),
+  require('@tailwindcss/aspect-ratio'),
+]
